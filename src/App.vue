a01687989a1fa9186be70b37a311b823f4a0bb52
Zhekun Tong <1085702426@qq.com>
2023-03-17 09:52:06 +0800

    tzks

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: Zhekun Tong 1085702426@qq.com
+ * @Date: 2023-03-17 09:27:42
+ * @LastEditors: Zhekun Tong 1085702426@qq.com
+ * @LastEditTime: 2023-03-17 09:35:03
+ * @FilePath: \tzk-project\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+     <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
