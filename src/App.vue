ca4d0cfdff814abc93715e5a39cb6d7a1959666c
Zhekun Tong <1085702426@qq.com>
2023-03-17 10:02:59 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
  * @Author: Zhekun Tong 1085702426@qq.com
  * @Date: 2023-03-17 09:27:42
  * @LastEditors: Zhekun Tong 1085702426@qq.com
- * @LastEditTime: 2023-03-17 09:35:03
+ * @LastEditTime: 2023-03-17 09:58:18
  * @FilePath: \tzk-project\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 这是默认设置,1请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div id="app">
